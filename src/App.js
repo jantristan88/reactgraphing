8c0880a7f3a1eedf5f8f97fa9f3af6215c725fd9
Jan Tristan <jan.tristan@gmail.com>
2019-04-30 21:21:01 -0700

    switched from checkbox to buttons

--- src/App.js
+++ src/App.js
@@ -1,167 +1,52 @@
-// import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-// import Checkbox from './Checkbox'
-// import {render} from 'react-dom'
-
-// const dataPoints =[];
-
-
-// class App extends Component {
-//   state = {
-//     party: '',
-//     year: '',
-//     number: '',
-//     bar: '', 
-//     isLoading:false,
-//     checkedRepublican: false,
-//     checkedDemocrat: false,
-//     checkedOther: false,
-
-//   }
-
-
-//   doFetch = () => {
-//     console.log('Hitting refresh');
-//     this.setState({
-//       isLoading: true,
-//     });
-//     fetch("./floridadata.json")
-//       .then(data => {
-//         return data.json()}) 
-//       .then(data => {
-//         console.log("Got the data!", data);
-
-//       this.setState({
-//         data: data,
-//         party: data.party,
-//         number: data.number,
-//         height: Math.round(data.number/5000000 * 100),
-//         year: Math.round(data.year),
-//         isLoading: false,
-//       });
-//     });
-
-//   }
-
-//   handleCheckboxChangeRepublican = (ev) =>
-//     // this.setState ({checked: ev.target.checked})
-//     this.setState ({checkedRepublican: true,
-//                     // party: data.republican})
-
-//   handleCheckboxChangeDemocrat = (ev) =>
-//     // this.setState ({checked: ev.target.checked})
-//     this.setState ({checkedDemocrat: true})
-
-//   // handleCheckboxChangeOther = (ev) =>
-//   //   this.setState ({checked: ev.target.checked})
-
-//   // gatherdataRepublican = (ev) =>
-//   //   this.setState ({party: data.republican})
-
-//   // componentDidMount = (ev) => {
-//   //   this.doFetch();
-//   // }
-
-
-
-//   onRefresh = () => {
-//     console.log('hitting refresh', this.state);
-//     fetch("./data/floridadata.json")
-//       .then(response => response.json())
-//       .then(data => {
-//         console.log('receiving data', data);
-//       })
-//   }
-
-//   render() {
-//     const options = {
-//       title: {
-//         text: "Florida Party Affiliation from 1972 to 2006"
-//       },
-//       axisY: {
-//         title: "% out of 5,000,000",
-//         includeZero: true
-//       },
-//       data: [{
-//         type: "bar",
-//         xValueFormatString: "YYYY",
-//         yvalueFormatString: "#,###,###",
-//         dataPoints: dataPoints,
-//       }]
-//     }
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <p>
-//             Edit <code>src/App.js</code> and save to reload.
-//           </p>
-//           <a
-//             className="App-link"
-//             href="https://reactjs.org"
-//             target="_blank"
-//             rel="noopener noreferrer"
-//           >
-//             Learn React
-//           </a>
-//         </header>
-
-//         <div style={{ fontFamily: 'system-ui' }}>
-//           <label>
-//             <Checkbox
-//               checked={this.state.checkedRepublican}
-//               onChange={this.handleCheckboxChangeRepublican}
-//               // this.state.checkedRepublican ? data.filter(function(item){
-//               //   return item.party === "Republican" && item.number }) : []
-        
-              
-//             />
-//             <span style={{ marginLeft:8 }}>Republican</span>
-//           </label>
-
-//           <label>
-//             <Checkbox
-//               checked={this.state.checkedDemocrat}
-//               onChange={this.handleCheckboxChangeDemocrat}
-//             />
-//             <span style={{ marginLeft:8 }}>Democrat</span>
-//           </label>
-
-//           <label>
-//             <Checkbox
-//               checked={this.state.checked}
-//               onChange={this.handleCheckboxChangeOther}
-//             />
-//             <span style={{ marginLeft:8 }}>Others</span>
-//           </label>
-//         </div> 
-
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
-
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import PropTypes from 'prop-types';
-import checkboxes from './checkboxes';
-import Checkbox from './Checkbox';
+import Tabs from './components/Tabs/Tabs.js';
 
 class App extends Component {
 
   state = {
     data: [],
     isLoading: false,
+    tabs: ['Republican', 'Democrat', 'Other'],
+    currentTab: 'Republican',
+    showRepublicans: false,
+    showDemocrats: true,
+    showOther: true,
+    Party: '',
   }
 
   componentDidMount(){
     this.doFetch()
   }
 
+  toggleRepublicans() {
+    console.log('elephant');
+    this.setState({
+      showRepublicans: true,
+      Party: 'Republican',
+    })
+    console.log(this.state.showRepublicans);
+      // if (!this.props.)
+    // if this.state.showRepublicans === false
+    //   then set style="display: none"
+      // if this.state.showRepublicans === true
+      //   then set style
+  }
+
+  toggleDemocrats() {
+    console.log('donkey');
+    this.setState({
+      showDemocrats: !this.state.showDemocrats
+    })
+  }
+
+  toggleOthers() {
+    console.log('leaf');
+    this.setState({
+      showOther: !this.state.showOther
+    })
+  }
 
   doFetch(){
     console.log('Hitting refresh');
@@ -176,6 +61,7 @@ class App extends Component {
 
       this.setState({
         data: data,
+
       });
     });
 
@@ -195,7 +81,7 @@ class App extends Component {
         <div className="Container-graph">
           {this.state.data.map(function(entry){
 
-
+          const party = entry.Party;
           const number = entry.Number;
           const height = Math.round(entry.Number/5000000*100);
 
@@ -203,8 +89,19 @@ class App extends Component {
 
             return (<p className="bar" style={{height: heightStyle}}>{entry["Number"]}</p>)
           })}
+          
         </div>
+
+        <div>
+          <Tabs
+            tabs={this.state.tabs}
+            onChange={this.toggleRepublicans.bind(this)} />
+        </div>
+      
       </div>
+
+      
+      
     );
   }
 }
